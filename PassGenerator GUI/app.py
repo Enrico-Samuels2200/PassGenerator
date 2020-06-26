fa25f91d224834f904681bc88dba165312e082c8
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2020-06-26 19:34:02 +0200

    Made updates to files.
    I updated the app.py in "/PassGenerator GUI/". 
    I've added a main() function and included (if __name__ == "__main__") statement.
    I added the ability to add symbols to the password as well.
    
    Due to the changes I recreated the pass_gen.exe.

--- PassGenerator GUI/app.py
+++ PassGenerator GUI/app.py
@@ -13,26 +13,37 @@ def generate_password():
 
   while pass_index < 12:
     # Genrates a value between 0 and 1 randomly.
-    pass_val = random.randint(0,1)
+    pass_val = random.randint(0,2)
   
-    # If value = 0 the password a random char will be added to the password.
+    # If value = 0 a random char will be added to the password.
     if pass_val == 0:
       new_char = random.choice(string.ascii_letters)
       password += new_char
 
-    # If value = 1 the password a random int will be added to the password.
+    # If value = 1 a random int will be added to the password.
     elif pass_val == 1:
       new_int = random.randint(0, 9)
       password += str(new_int)
+    
+    # If value = 2 a random symbol will be added to the password.
+    elif pass_val == 2:
+      new_char = random.choice(string.punctuation)
+      password += str(new_char)
     
     # Allows the index value to raise. Once this value is equal to 12 the while loop will break and we will have a password of the length 12.
     pass_index += 1
     
   return password
 
-# Starts the main.html on server 127.0.0.1:8000 with the size of the window.
-eel.start('main.html', size=(768, 850), block=False)
 
-# Keeps the app running after start up.
-while True:
-  eel.sleep(10)
+def main():
+  # Starts the main.html on server 127.0.0.1:8000 with the size of the window.
+  eel.start('main.html', size=(768, 850), block=False)
+
+  # Keeps the app running after start up.
+  while True:
+    eel.sleep(10)
+
+# Calls the main function if ran locally and not imported.
+if __name__ == "__main__":
+  main()
--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,5 @@
 import random
 import string
-
 # Generates a random password. 
 def generate_password():
   pass_index = 0
@@ -8,18 +7,23 @@ def generate_password():
 
   while pass_index < 12:
     # Genrates a value between 0 and 1 randomly.
-    pass_val = random.randint(0,1)
+    pass_val = random.randint(0,2)
   
-    # If value = 0 the password a random char will be added to the password.
+    # If value = 0 a random char will be added to the password.
     if pass_val == 0:
       new_char = random.choice(string.ascii_letters)
       password += new_char
 
-    # If value = 1 the password a random int will be added to the password.
+    # If value = 1 a random int will be added to the password.
     elif pass_val == 1:
       new_int = random.randint(0, 9)
       password += str(new_int)
     
+    # If value = 2 a random symbol will be added to the password.
+    else:
+      new_sym = random.choice(string.punctuation)
+      password += str(new_sym)
+
     # Allows the index value to raise. Once this value is equal to 12 the while loop will break and we will have a password of the length 12.
     pass_index += 1
     
@@ -29,39 +33,6 @@ def main():
   new_password = generate_password()
   print(new_password)
 
-# Calls the main function if ran locally.
-if __name__ == "__main__":
-  main()import random
-import string
-
-# Generates a random password. 
-def generate_password():
-  pass_index = 0
-  password = ''
-
-  while pass_index < 12:
-    # Genrates a value between 0 and 1 randomly.
-    pass_val = random.randint(0,1)
-  
-    # If value = 0 the password a random char will be added to the password.
-    if pass_val == 0:
-      new_char = random.choice(string.ascii_letters)
-      password += new_char
-
-    # If value = 1 the password a random int will be added to the password.
-    elif pass_val == 1:
-      new_int = random.randint(0, 9)
-      password += str(new_int)
-    
-    # Allows the index value to raise. Once this value is equal to 12 the while loop will break and we will have a password of the length 12.
-    pass_index += 1
-    
-  return password
-
-def main():
-  new_password = generate_password()
-  print(new_password)
-
-# Calls the main function if ran locally.
+# Calls the main function if ran locally and not imported.
 if __name__ == "__main__":
   main()
